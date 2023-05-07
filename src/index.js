04c6d37bb836d5929c0ef08f47af39b5fe8cd1d5
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-05-07 16:26:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goose_track">
+        <BrowserRouter basename="/react_node_fs_59_ElenaOds">
           <App />
         </BrowserRouter>
       </PersistGate>
